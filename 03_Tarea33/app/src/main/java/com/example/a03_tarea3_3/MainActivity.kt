47590e18564586ab2d15dafcbdb1da2156d6f37c
pedrocornejocarvajal <75272975+pedrocornejocarvajal@users.noreply.github.com>
2022-10-31 11:47:53 +0100

    31/10/2022

---
+++ 03_Tarea33/app/src/main/java/com/example/a03_tarea3_3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a03_tarea3_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
